a3267870770ce5a58e7448d18a12844a4a912742
yurimachida0207 <machidayuuri@yahoo.co.jp>
2020-07-28 17:03:47 +0900

    あ

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
   
   root 'toppages#index'
+  resources :login, only: [:index, :new]
   resources :toppages,only: :index
   resources :new_member, only: :index
   resources :toppages, only: :index
   resources :transactions, only: :index
   resources :mypages, only: :index
-
-  get "new_member/index"
-  get "transactions/buy"
-  get "mypages/index"
-  get "mypages/logout"
-  get "mypages/payment"
 
 end
