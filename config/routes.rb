f2e98a464edceb6d0fc59614a1959e5d6433b9a2
Toshikatsu08 <gaffai-soccer@outlook.jp>
2020-07-28 23:15:44 +0900

    Deleted code and files.
    Deleted "resources :toppages, only: :index" because of duplication.
    
    Deleted items.scss files for test..

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   resources :login, only: [:index, :new]
   resources :toppages,only: :index
   resources :new_member, only: :index
-  resources :toppages, only: :index
   resources :transactions, only: :index
   resources :mypages, only: :index
   resources :show_items,only: :index
