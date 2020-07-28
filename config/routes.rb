671fc5743d3a259f88c7a19a4c69429f2d6df06d
shinozaki-0722 <tellyourworld0722@gmail.com>
2020-07-28 22:09:40 +0900

    payment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   root 'items#index'
-  get "transactions/buy"
-  get "mypages/index"
-  get "mypages/logout"
-  get "mypages/payment"
+
+  resources :transactions do
+    get 'buy'
+  end
+
+  resources :mypages do
+    collection do
+      get 'index'
+      get 'logout'
+      get 'payment'
+    end
+  end
 end
