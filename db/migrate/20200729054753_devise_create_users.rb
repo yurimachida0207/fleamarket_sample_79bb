828d5e4a65091ab2822e1467930d5fab94fdc0eb
Toshikatsu08 <gaffai-soccer@outlook.jp>
2020-07-29 14:54:30 +0900

    Installed 'devise' and created user model  and colummn.

---
+++ db/migrate/20200729054753_devise_create_users.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :nickname,           null: false
+      t.string :email,              null: false, unique: true
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_kana,     null: false
+      t.string :first_name_kana,    null: false
+      t.integer :birthdate_year,    null: false
+      t.integer :birthdate_month,   null: false
+      t.integer :birthdate_day,     null: false
+      t.integer :phone_number,      null: false, unique: true
+      t.string :address_last_name,  null: false
+      t.string :address_first_name, null: false
+      t.string :address_last_name_kana,  null: false
+      t.string :address_first_name_kana, null: false
+      t.string :address_number,      null: false
+      t.integer :address_prefecture, null: false, default: 0
+      t.string :address_name,        null: false
+      t.string :address_block,       null: false
+      t.string :address_building
+      t.integer :address_phone_number
+      t.string :encrypted_password,  null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      # t.integer  :sign_in_count, default: 0, null: false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.string   :current_sign_in_ip
+      # t.string   :last_sign_in_ip
+
+      ## Confirmable
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## Lockable
+      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
+  end
+end
