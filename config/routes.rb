7d0b08aada63891ab08de8280f39f1f30471c3b3
yurimachida0207 <machidayuuri@yahoo.co.jp>
2020-07-29 11:40:28 +0900

    my_page_show

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-  root 'toppages#index'
-
+  # root 'toppages#index'
+  root 'mypages#show'
   resources :login, only: [:index, :new]
   resources :toppages,only: :index
   resources :new_member, only: :index
   resources :toppages, only: :index
   resources :transactions, only: :index
-  resources :mypages, only: :index
+  resources :mypages, only: [:index, :show]
   resources :show_items,only: :index
+
 end
