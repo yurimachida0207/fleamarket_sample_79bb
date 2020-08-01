e3f2538620d6b73174a29cf8595ef6029685cd22
Toshikatsu08 <gaffai-soccer@outlook.jp>
2020-08-01 15:01:05 +0900

    Add devise controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  
   root 'toppages#index'
 
   resources :transactions do
