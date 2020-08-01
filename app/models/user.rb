0c123cf98e7633ecbdaf4ded6fc90b78a00eb9ef
Toshikatsu08 <gaffai-soccer@outlook.jp>
2020-08-01 13:19:52 +0900

    Add column and validates.

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
+end
