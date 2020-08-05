a64b6d6b9faf079442ec96597dc7ae64960d3dae
Toshikatsu08 <gaffai-soccer@outlook.jp>
2020-08-05 16:00:08 +0900

    Created page of new items index.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create, :get_category_children, :get_category_grandchildren]
 
   def index
-    @items = Item.all
+    @items = Item.includes(:images).order('created_at DESC')
   end
 
   def new
