b539723796f4ab92735ff59e98c44c64426043f9
Toshikatsu08 <gaffai-soccer@outlook.jp>
2020-08-06 09:55:28 +0900

    Update toppages_controller.rb

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,5 @@
 class ToppagesController < ApplicationController
   def index
     @items = Item.includes(:images).order('updated_at DESC').first(4)
-    # @rand_items = Item.includes(:images).order("RAND()").limit(3)
   end
 end
