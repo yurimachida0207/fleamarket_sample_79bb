ef982fb427ad17d8dc2048aa30343fca42209e17
Toshikatsu08 <gaffai-soccer@outlook.jp>
2020-08-07 17:46:53 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,5 @@
-aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+aws_access_key_id: ENV['AWS_ACCESS_KEY_ID']
+aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
 
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
